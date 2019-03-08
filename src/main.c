604783235d9bdef12d1bc14996305313bcffddff
DelaneyJL <48161521+DelaneyJL@users.noreply.github.com>
2019-03-07 20:29:47 -0500

    Update main.c
    Extension of the function grabbing_botguy

--- src/main.c
+++ src/main.c
@@ -6,9 +6,10 @@
 #define sweeper 3
 
 #define lift_up 1065
+#define lift_down 0
 
-#define claw_open 1442
-#define claw_close 873
+#define claw_open 1470
+#define claw_close 556
 
 //Included Libraries:
 #include <kipr/botball.h>
@@ -17,32 +18,45 @@
 
 //Functions
 void grabbing_botguy();
+void start();
+void stop();
 
 int main()
 {
-    create_connect();
-    printf ("blue\n");
+    start();
     grabbing_botguy();
-    /*enable_servo(1);
-    printf ("blue\n");
-    servo(1, claw_up);
-    printf ("blue\n");
-    disable_servo(1);*/
+    stop();
+    return 0;
+}
+
+void start() {
+    create_connect();
+    printf("blue\n");
+}
+
+void stop() {
     create_stop();
     create_disconnect();
-    return 0;
 }
 
 void grabbing_botguy() {
     enable_servo(claw);
     enable_servo(lift);
     servo(lift, lift_up);
+    msleep(1000);
     servo(claw, claw_open);
+    msleep(1000);
     create_drive_direct(70, 70);
-    msleep(6500);
+    msleep(8000);
     create_stop();
     servo(claw, claw_close);
+    msleep(1000);
     create_drive_direct(-70, -70);
     msleep(3000);
+    create_stop();
+    servo(lift, lift_down);
+    msleep(1000);
+    servo(claw, claw_open);
+    msleep(1000);
     create_stop();
 }
